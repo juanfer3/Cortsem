82409d34c5c3c69f59bb5a461056233f2b5e2573
Juan Fernando Villadiego Madera <jfvilladiego2@misena.edu.co>
2017-06-10 22:00:38 +0000

    idioma bootstrap y simple form

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -14,3 +14,18 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
 # end
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.clear
+ 
+  inflect.plural(/([^djlnrs])([A-Z]|_|$)/, '\1s\2')
+  inflect.plural(/([djlnrs])([A-Z]|_|$)/, '\1es\2')
+  inflect.plural(/(.*)z([A-Z]|_|$)$/i, '\1ces\2')
+ 
+  inflect.singular(/([^djlnrs])s([A-Z]|_|$)/, '\1\2')
+  inflect.singular(/([djlnrs])es([A-Z]|_|$)/, '\1\2')
+  inflect.singular(/(.*)ces([A-Z]|_|$)$/i, '\1z\2')
+ 
+  inflect.irregular('el', 'los')
+  inflect.irregular('articulo', 'articulos')
+  inflect.irregular('user', 'users')    
+end
